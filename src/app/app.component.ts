f43eaa7058f930671ef94b61a1c1cd90d4d8cc56
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-11 01:57:45 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  value: number = 1;
+  value: number = 0;
+  newValue: number = 0;
+
+  public handleNewCountEvent($event: number) {
+    if ($event) this.newValue = $event;
+  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,10 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
   @Input() count: number = -1;
+  @Output() newCountEvent = new EventEmitter<number>();
 
   // We increment the counter on init
   ngOnInit() {
     this.increment();
+    this.newCountEvent.emit(this.count);
   }
 
   increment() {
